46ca134a8b48f801440b8372890a9904c6df436d
heren <heren1heren@gmail.com>
2025-03-20 10:42:05 -0600

    finished overall and is clearning up

--- DMIT1530/dmit-1530-assignment-2-Heren-Tran/js/main.js
+++ DMIT1530/dmit-1530-assignment-2-Heren-Tran/js/main.js
@@ -1,6 +1,13 @@
 const menuBtn = document.querySelector('.menu-btn');
+const dropdownBtn = document.querySelector('.shop-drop-down-btn');
+const dropdownMenu = document.querySelector('.shop-drop-down-menu');
 const nav = document.querySelector('nav');
 menuBtn.addEventListener('click', () => {
-  console.log('click');
+
   nav.classList.toggle('visible');
+});
+
+dropdownBtn.addEventListener('click', () =>{
+  dropdownMenu.classList.toggle('visible');
+
 });
